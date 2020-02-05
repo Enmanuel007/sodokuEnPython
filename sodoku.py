c2ba41e6bc245ede1aae9f8de2178d3d4435ed72
Enmanuel <eochoa@umariana.edu.co>
2020-02-05 08:25:08 -0500

    if de condición de suma de números en fila y columna

--- sodoku.py
+++ sodoku.py
@@ -1,4 +1,6 @@
 
+
+
 af0c0, af0c1, af0c2, af0c3 =    5,  None,  None,  18
 af1c0, af1c1, af1c2, af1c3 = None,  None,  None,  11
 af2c0, af2c1, af2c2, af2c3 = None,  None,  None,  16
@@ -27,11 +29,54 @@ b = np.array([
     [bf2c0,  bf2c1,  bf2c2,  bf2c3],
     [bf3c0,  bf3c1,  bf3c2,  bf3c3]])
 
-
+#Condiciones números dígitos 
 if 0<bf0c1<=9 and 0<bf0c2<=9 and 0<bf1c0<=9 and 0<bf1c1<=9 and 0<bf1c2<=9 and 0<bf2c0<=9 and 0<bf2c1<=9 and 0<bf2c2<=9:
-    print (b)
-    print ("El sodoku está resuelto correctamente")
+
+    #Condiciones suma de fila
+    if bf0c0==5 and bf0c1==4 and bf0c2==9:
+        bf0c3 = (bf0c0+  bf0c1+ bf0c2);
+        
+        if bf1c0==8 and bf1c1==2 and bf1c2==1:
+            bf1c3 = (bf1c0+  bf1c1+ bf1c2);
+            
+            if bf2c0==3 and bf2c1==6 and bf2c2==7:
+                bf2c3 = (bf2c0+  bf2c1+ bf2c2);
+               
+               
+                #Condiciones suma de columnas
+                if bf0c0==5 and bf1c0==8 and bf2c0==3:
+                    bf3c0 = (bf0c0+  bf1c0+ bf2c0);
+                    
+
+                    if bf0c1==4 and bf1c1==2 and bf2c1==6:
+                        bf3c1 = (bf0c1+  bf1c1+ bf2c1);
+                        
+                        if bf0c2==9 and bf1c2==1 and bf2c2==7:
+                            bf3c2 = (bf0c2+  bf1c2+ bf2c2);
+                            print (b)
+                        else:
+                            print (a)
+                            print ("Error el número no aplica para este ejercicio de sodoku")
+
+                    else:
+                        print (a)
+                        print ("Error el número no aplica para este ejercicio de sodoku")
+
+                else:
+                    print (a)
+                    print ("Error el número no aplica para este ejercicio de sodoku")
+
+
+            else:
+                print (a)
+                print ("Error el número no aplica para este ejercicio de sodoku")
+
+        else:
+            print (a)
+            print ("Error el número no aplica para este ejercicio de sodoku")
+    else:
+        print (a)
+        print ("Error el número no aplica para este ejercicio de sodoku")
 else:
     print (a)
     print ("Error el número no aplica para este ejercicio de sodoku")
-
