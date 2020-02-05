03287969e19aedd5dd65d0ac7cc9ba3876f058f2
Enmanuel <eochoa@umariana.edu.co>
2020-02-05 03:48:26 -0500

    Creación de matriz guía sodoku

--- sodoku.py
+++ sodoku.py
@@ -1,24 +1,13 @@
+f0c0, f0c1, f0c2, f0c3 =    5,  None,  None,  18
+f1c0, f1c1, f1c2, f1c3 = None,  None,  None,  11
+f2c0, f2c1, f2c2, f2c3 = None,  None,  None,  16
+f3c0, f3c1, f3c2, f3c3 =   16,    12,    17,  14
 import numpy as np
+
 a = np.array([
-    [2, 2, 2, 2],
-    [2, 2, 2, 2],
-    [2, 2, 2, 2],
-    [2, 2, 2, 2]])
+    [f0c0,  f0c1,  f0c2,  f0c3],
+    [f1c0,  f1c1,  f1c2,  f1c3],
+    [f2c0,  f2c1,  f2c2,  f2c3],
+    [f3c0,  f3c1,  f3c2, f3c3]])
+
 print (a)
-
-
-
-
-rf1 = 18
-rf2 = 11
-rf3 = 16
-rf4 = 14
-
-rc1 = 16
-rc2 = 12
-rc3 = 17
-rc4 = 14
-num1f1c1 = 5
-
-
-
